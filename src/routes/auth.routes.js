dc4728a6a61f1c4a769ad9adc82e9cef9383bb22
Joaquin Wisgikl <joaquinwisgikl@gmail.com>
2024-05-08 13:58:27 -0300

    Ruta logout y autenticacion por token (jwt)

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  profile,
+  register,
+} from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.post("/logout", logout);
+router.get("/profile", authRequired, profile);
 export default router;
