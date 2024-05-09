9f6b3d670bca0c1793e0d2d2c718c79946724efd
Joaquin Wisgikl <joaquinwisgikl@gmail.com>
2024-05-09 17:56:14 -0300

    Form con el endpoint del backy manejo de errores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { RegisterPage } from "./pages/RegisterPage.jsx";
+import { LoginPage } from "./pages/LoginPage.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/login" element={<h1>Login Page</h1>} />
-        <Route path="/register" element={<h1>Register Page</h1>} />
-        <Route path="/task" element={<h1>Task Page</h1>} />
-        <Route path="/add-task" element={<h1>New Page</h1>} />
-        <Route path="/tasks/:id" element={<h1>Update Page</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+          <Route path="/add-task" element={<h1>New Page</h1>} />
+          <Route path="/tasks/:id" element={<h1>Update Page</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 export default App;
